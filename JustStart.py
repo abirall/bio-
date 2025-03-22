bb8ec68ce7b9773a483725322440a920b465c9c2
TasrifMahin <mahintasrifhasan@gmail.com>
2025-03-22 23:03:57 +0600

    one

---
+++ JustStart.py
@@ -0,0 +1,50 @@
+name = ("Abir")
+age = 24
+price = 39.99
+
+print ("My name is", name)
+print ("My age is", age)
+print ("My hourly rate", price)
+
+print (type (name))
+print (type (age))
+print (type (price))
+
+#sun
+a=6
+b=9
+sum=b-a
+
+print(sum)
+
+print (a+b)
+print (a-b)
+print (a*b)
+print (a%b)
+print (a/b)
+print (a**b)
+
+
+print (not True)
+print (not (b>a))
+
+print ("write a prgram to squre print area!")
+
+#side = float (input ("enter the side of the squre:"))
+#sum =side **2
+#print ("the area of the squre is :" , sum)
+
+
+#write a program two floating number avrage!
+
+#t= float (input( "enter the first number:"))
+#y= float (input( "enter the second number:"))
+#sum= (t+y)/2
+#print ("the avrage of the two number is :", sum)
+
+#write a program two input numbers a and b print true if a is greater than b otherwise false!
+
+a= int (input ("enter first Number:"))
+b= int (input ("enter second Number:"))
+
+print (a>=b)
